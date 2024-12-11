bd999fccd74cc87d7902365e32ee8da300ae56a9
Davi257-cmd <golon54204@gmail.com>
2024-12-10 23:38:21 -0300

    desafio 4

---
+++ Curso 2/js-curso-2-aula1/js-curso-2-aula_-/desafio4.js
@@ -0,0 +1,26 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+console.log(linguagensDeProgramacao);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaDeNomes = ['Alice', 'Bruno', 'Carlos'];
+console.log(listaDeNomes[0]); 
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaDeNomes = ['Alice', 'Bruno', 'Carlos'];
+console.log(listaDeNomes[1]); 
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaDeNomes = ['Alice', 'Bruno', 'Carlos'];
+console.log(listaDeNomes[listaDeNomes.length - 1]);
+
+
